298470b71add30b2045dd8e8717404cf4b5495da
lgq <liuguoqing@hzxituan.com>
2020-12-02 00:32:29 +0800

    add output

---
+++ gopl.io/ch3/output/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	medals := []string{"aa", "bb", "cc"}
+	for i := len(medals) - 1; i >= 0; i-- {
+		fmt.Println(medals[i])
+	}
+
+	o := 0666
+	fmt.Printf("%d %[1]o %#[1]o\n", o)
+
+	x := int64(0xdeadbeef)
+	fmt.Printf("%d %[1]x %#[1]x %#[1]X \n", x)
+
+	ascii := 'a'
+	unicode := '国'
+	newline := '\n'
+	fmt.Printf("%d %[1]c %[1]q\n", ascii)
+	fmt.Printf("%d %[1]c %[1]q\n", unicode)
+	fmt.Printf("%d %[1]q\n", newline)
+}
