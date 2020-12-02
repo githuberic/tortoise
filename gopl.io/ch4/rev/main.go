d883ef6e0ef71d76e51ba66963a3900d6da7e7b8
lgq <liuguoqing@hzxituan.com>
2020-12-03 01:00:07 +0800

    add rev

---
+++ gopl.io/ch4/rev/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func reverse(arr []int) {
+	for i, j := 0, len(arr)-1; i < j; i, j = i+1, j-1 {
+		arr[i], arr[j] = arr[j], arr[i]
+	}
+}
+
+func main() {
+	arr := [...]int{0, 1, 2, 3, 4, 5}
+	reverse(arr[:])
+	fmt.Println(arr)
+
+	arr1 := []int{0, 1, 2, 3, 4, 5}
+	reverse(arr1[:2])
+	fmt.Println(arr1)
+	reverse(arr1[2:])
+	fmt.Println(arr1)
+	reverse(arr1)
+	fmt.Println(arr1) // "[2 3 4 5 0 1]"
+
+	input := bufio.NewScanner(os.Stdin)
+outer:
+	for input.Scan() {
+		var ints []int
+		for _, s := range strings.Fields(input.Text()) {
+			x, err := strconv.ParseInt(s, 10, 64)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				continue outer
+			}
+			ints = append(ints, int(x))
+		}
+		reverse(ints)
+		fmt.Printf("%v\n", ints)
+	}
+}
