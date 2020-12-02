ed07f8f9554ef8ce10006eea42ee76a546f23c1f
lgq <liuguoqing@hzxituan.com>
2020-12-03 01:32:55 +0800

    add mapbasic

---
+++ gopl.io/ch4/mapbasic/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	ages := make(map[string]int)
+	ages["alice"] = 31
+	ages["charlie"] = 34
+	ages["eric"] = 35
+
+	for name, age := range ages {
+		fmt.Printf("%s\t%d\n", name, age)
+	}
+
+	var names []string
+	for name := range ages {
+		names = append(names, name)
+	}
+	sort.Strings(names)
+	for _, name := range names {
+		fmt.Printf("%s\t%d\n", name, ages[name])
+	}
+}
