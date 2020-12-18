6fdcda0bf18fb97d57b3a0aa54ab37b3f76e81d0
lgq <liugq@weipaitang.com>
2020-12-19 00:31:43 +0800

    add ch9-memo1

---
+++ gopl.io/ch9/memo1/memo_test.go
@@ -0,0 +1,20 @@
+package memo_test
+
+import (
+	"testing"
+	memo "tortoise/gopl.io/ch9/memo1"
+	"tortoise/gopl.io/ch9/memotest"
+)
+
+var httpGetBody = memotest.HTTPGetBody
+
+func Test(t *testing.T) {
+	m := memo.New(httpGetBody)
+	memotest.Sequential(t, m)
+}
+
+// NOTE: not concurrency-safe!  Test fails.
+func TestConcurrent(t *testing.T) {
+	m := memo.New(httpGetBody)
+	memotest.Concurrent(t, m)
+}
