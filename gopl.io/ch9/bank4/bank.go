6f992afbb27caedf79705c9003ac8ad4203b523e
lgq <liugq@weipaitang.com>
2020-12-19 00:10:05 +0800

    ch9-bank4

---
+++ gopl.io/ch9/bank4/bank.go
@@ -0,0 +1,35 @@
+package bank
+
+import "sync"
+
+var (
+	mu      sync.Mutex // guards balance
+	balance int
+)
+
+func Withdraw(amount int) bool {
+	mu.Lock()
+	defer mu.Unlock()
+	deposit(-amount)
+	if balance < 0 {
+		deposit(amount)
+		return false // 余额不足
+	}
+	return true
+}
+
+func Deposit(amount int) {
+	mu.Lock()
+	defer mu.Unlock()
+	deposit(amount)
+}
+
+func Balance() int {
+	mu.Lock()
+	defer mu.Unlock()
+	return balance
+}
+
+func deposit(amount int) {
+	balance += amount
+}
