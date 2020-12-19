09477e7799b6ca40a42cff85b38c372d7372721e
lgq <liugq@weipaitang.com>
2020-12-19 14:28:55 +0800

    add ch1-echo1/echo2 commend

--- gopl.io/ch1/echo1/main.go
+++ gopl.io/ch1/echo1/main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	var s, sep string
 	// := 符号用于短变量声明
+	// os.Args是一个字符串slice,动态容量的顺组数组，可以通过s[i]来访问单个元素
 	for i :=1; i < len(os.Args); i++ {
 		s += sep + os.Args[i]
 		sep = " "
--- gopl.io/ch1/echo2/main.go
+++ gopl.io/ch1/echo2/main.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	s, sep := "", ""
+	// 每次迭代 range都会产生一对值 索引/这个索引处元素的值，我们不需要索引时，通过空标识符 _ 来解决
 	for _, arg := range os.Args[1:] {
 		s += sep + arg
 		sep = " "
