2ddc213a3b99c74a3e68d681647b322cafd40afa
lgq <liugq@weipaitang.com>
2020-12-20 12:15:40 +0800

    add ch2-echo4 commend

--- gopl.io/ch2/echo4/main.go
+++ gopl.io/ch2/echo4/main.go
@@ -9,6 +9,8 @@ import (
 	"strings"
 )
 
+// flag.Bool  函数创建一个新的boolean标识变量，三个参数 标识的名字(n),变量的默认值false,以及当用户提供非法标识、非法参数、-h/-help参数时输出的信息
+// 变量sep/n是指向标识变量的指针，必须通过*set/*n来访问
 var n = flag.Bool("n",false,"omit trailing newline")
 var sep = flag.String("s"," ","separator")
 
