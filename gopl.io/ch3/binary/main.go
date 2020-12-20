ee00575207dccb65b0069950a60e3a74789ad08f
lgq <liugq@weipaitang.com>
2020-12-20 16:06:12 +0800

    add ch3-output/binary commend

--- gopl.io/ch3/binary/main.go
+++ gopl.io/ch3/binary/main.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+/**
+to do...
+ */
 func main() {
 	var x uint8 = 1<<1 | 1<<5
 	var y uint8 = 1<<1 | 1<<2
@@ -11,6 +14,10 @@ func main() {
 	fmt.Printf("%08b,%d\n", y, y)
 	fmt.Printf("%08b,%d\n", x&y, x&y)
 	fmt.Printf("%08b,%d\n", x|y, x|y)
-	fmt.Printf("%08b,%d\n", x^y, x^y)
+	// 按位异或的3个特点:
+	// (1) 0^0=0,0^1=1  0异或任何数＝任何数
+	// (2) 1^0=1,1^1=0  1异或任何数=任何数取反
+	fmt.Printf("%08b,%d\n", x^y, x^y) //^按位异或(XOR)，若作为1元操作符,它标识按位取反或按位取补,0和1异或0都不变，异或1则取反
+
 	fmt.Printf("%08b,%d\n", x&^y, x&^y)
 }
--- gopl.io/ch3/output/main.go
+++ gopl.io/ch3/output/main.go
@@ -8,7 +8,10 @@ func main() {
 		fmt.Println(medals[i])
 	}
 
+	// 八进制
 	o := 0666
+	// %后面的副词[1]告知printf重复使用第一个操作数
+	// %o %x %X之前的副词#告知printf输出相应的前缀0 0x 0X
 	fmt.Printf("%d %[1]o %#[1]o\n", o)
 
 	x := int64(0xdeadbeef)
@@ -17,6 +20,7 @@ func main() {
 	ascii := 'a'
 	unicode := '国'
 	newline := '\n'
+	// %c 输出文字符号，输出带单引号 %q
 	fmt.Printf("%d %[1]c %[1]q\n", ascii)
 	fmt.Printf("%d %[1]c %[1]q\n", unicode)
 	fmt.Printf("%d %[1]q\n", newline)
