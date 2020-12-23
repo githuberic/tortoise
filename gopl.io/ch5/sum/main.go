4dd58b590122791e588571da13242a9be42a6ef8
lgq <liugq@weipaitang.com>
2020-12-23 22:43:52 +0800

    add ch5-sum

---
+++ gopl.io/ch5/sum/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+/**
+变长函数
+ */
+func sum(vals ...int) int {
+	total := 0
+	for _, val := range vals {
+		total += val
+	}
+	return total
+}
+
+func main() {
+	//!+main
+	fmt.Println(sum())           //  "0"
+	fmt.Println(sum(3))          //  "3"
+	fmt.Println(sum(1, 2, 3, 4)) //  "10"
+	//!-main
+
+	//!+slice
+	values := []int{1, 2, 3, 4}
+	fmt.Println(sum(values...)) // "10"
+	//!-slice
+}
