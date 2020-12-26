04e9d3d22c00153bd2d9bb0647822d1d63b2b812
lgq <liugq@weipaitang.com>
2020-12-26 23:59:27 +0800

    add ch12-search

---
+++ gopl.io/ch12/search/main.go
@@ -0,0 +1,33 @@
+package search
+
+import (
+	"fmt"
+	"net/http"
+	"log"
+	"tortoise/gopl.io/ch12/params"
+)
+
+// search implements the /search URL endpoint.
+func search(resp http.ResponseWriter, req *http.Request) {
+	var data struct {
+		Labels     []string `http:"l"`
+		MaxResults int      `http:"max"`
+		Exact      bool     `http:"x"`
+	}
+	data.MaxResults = 10 // set default
+	if err := params.Unpack(req, &data); err != nil {
+		http.Error(resp, err.Error(), http.StatusBadRequest) // 400
+		return
+	}
+
+	// ...rest of handler...
+	fmt.Fprintf(resp, "Search: %+v\n", data)
+}
+
+//!-
+
+func main() {
+	http.HandleFunc("/search", search)
+	log.Fatal(http.ListenAndServe(":12345", nil))
+}
+
