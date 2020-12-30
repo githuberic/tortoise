d8152ed62b429abf00396c3185750c7a7560b0b1
lgq <liugq@weipaitang.com>
2020-12-30 22:56:28 +0800

    add go_learning

---
+++ go_learning/ch3/type_test.go
@@ -0,0 +1,24 @@
+package type_test
+
+import "testing"
+
+type MyInt int64
+
+func TestImplicit(t *testing.T) {
+	var a int32 = 1
+	var b int64
+	b = int64(a)
+	var c MyInt
+	c = MyInt(b)
+	t.Log(a, b, c)
+}
+
+func TestPoint(t *testing.T) {
+	a := 1
+	// int类型指针
+	aPtr := &a
+	// 不支持指针运算
+	// aPtr = aPtr + 1
+	t.Log(a, aPtr)
+	t.Logf("%T %T", a, aPtr)
+}
