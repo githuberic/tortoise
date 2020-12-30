7a7a6e750ffb01e93068f9f2478f5273402e184e
lgq <liugq@weipaitang.com>
2020-12-30 23:24:45 +0800

    add ch5-opeator-operator-test

---
+++ go_learning/ch3/operator/operator_test.go
@@ -0,0 +1,29 @@
+package operator_test
+
+import "testing"
+
+func TestCompareArray(t *testing.T) {
+	a := [...]int{1, 2, 3, 4}
+	b := [...]int{1, 3, 2, 4}
+	// 长度不同的数组，不能比较
+	//	c := [...]int{1, 2, 3, 4, 5}
+	d := [...]int{1, 2, 3, 4}
+	t.Log(a == b)
+	//t.Log(a == c)
+	// 类型相同、长度相同
+	t.Log(a == d)
+}
+
+const (
+	Readable = 1 << iota
+	Writable
+	Executable
+)
+func TestBitClear(t *testing.T) {
+	a := 7 //0111
+	t.Log("a=,b=,c=",Readable,Writable,Executable)
+	// &^ 按位清零运算符
+	a = a &^ Readable
+	a = a &^ Executable
+	t.Log(a&Readable == Readable, a&Writable == Writable, a&Executable == Executable)
+}
