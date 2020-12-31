20f77a39d7ba6d0509e6ad4c7ce10c388060a998
lgq <liugq@weipaitang.com>
2020-12-31 23:37:30 +0800

    add ch4-func

---
+++ go_learning/ch4/func/func_test.go
@@ -0,0 +1,58 @@
+package func_test
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func returnMultiValues() (int, int) {
+	return rand.Intn(10), rand.Intn(20)
+}
+
+func slowFun(op int) int {
+	time.Sleep(time.Second * 1)
+	return op
+}
+
+func timeSpent(inner func(op int) int) func(op int) int {
+	return func(n int) int {
+		start := time.Now()
+		ret := inner(n)
+		fmt.Println("time spent:", time.Since(start).Seconds())
+		return ret
+	}
+}
+
+func TestFn(t *testing.T) {
+	a, _ := returnMultiValues()
+	t.Log(a)
+	tsSF := timeSpent(slowFun)
+	t.Log(tsSF(10))
+}
+
+// 可变长参数
+func Sum(ops ...int) int {
+	ret := 0
+	for _, op := range ops {
+		ret += op
+	}
+	return ret
+}
+
+func TestVarParam(t *testing.T) {
+	t.Log(Sum(1, 2, 3, 4))
+	t.Log(Sum(1, 2, 3, 4, 5))
+}
+
+func Clear() {
+	fmt.Println("Clear resources.")
+}
+
+func TestDefer(t *testing.T) {
+	defer Clear()
+	fmt.Println("Start")
+	// 即使panic defer仍然会运行
+	panic("err")
+}
