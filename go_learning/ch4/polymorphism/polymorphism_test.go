2c0e3a077e9c3d5af234d813aa0addf292dbf48e
lgq <liugq@weipaitang.com>
2021-01-01 19:33:55 +0800

    add ch4-polymorphism

---
+++ go_learning/ch4/polymorphism/polymorphism_test.go
@@ -0,0 +1,37 @@
+package polymorphism_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+type Code string
+type Programmer interface {
+	WriteHelloWorld() Code
+}
+
+type GoProgrammer struct {
+}
+
+func (p *GoProgrammer) WriteHelloWorld() Code {
+	return "fmt.Println(\"Hello World!\")"
+}
+
+type JavaProgrammer struct {
+}
+
+func (p *JavaProgrammer) WriteHelloWorld() Code {
+	return "System.out.Println(\"Hello World!\")"
+}
+
+func writeFirstProgram(p Programmer) {
+	fmt.Printf("%T %v\n", p, p.WriteHelloWorld())
+}
+
+func TestPolymorphism(t *testing.T) {
+	// p Programmer，指针类型
+	goProg := &GoProgrammer{}
+	javaProg := new(JavaProgrammer)
+	writeFirstProgram(goProg)
+	writeFirstProgram(javaProg)
+}
