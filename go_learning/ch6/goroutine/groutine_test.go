5fa7ac204c33aa47133c8370ef5520172dce2f31
lgq <liugq@weipaitang.com>
2021-01-01 20:56:39 +0800

    add ch6-goroutine

---
+++ go_learning/ch6/goroutine/groutine_test.go
@@ -0,0 +1,17 @@
+package groutine_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestGoroutine(t *testing.T) {
+	for i :=0; i < 10; i++ {
+		//
+		go func(i int) {
+			fmt.Println(i)
+		}(i) // 值传递，i值被复制了一份, 和其他go routine没有竞争关系
+	}
+	time.Sleep(time.Millisecond * 50)
+}
