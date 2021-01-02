e287afca4f48b31f67731c393e93aaa76f8a3a66
lgq <liugq@weipaitang.com>
2021-01-02 16:25:57 +0800

    add ch7-first-response

---
+++ go_learning/ch7/util_anyone_reply/first_response_test.go
@@ -0,0 +1,38 @@
+package concurrency
+
+import (
+	"fmt"
+	"runtime"
+	"testing"
+	"time"
+)
+
+func runTask(id int) string {
+	time.Sleep(10 * time.Millisecond)
+	return fmt.Sprintf("The result is from %d", id)
+}
+
+func FirstResponse() string {
+	numOfRunner := 10
+	// 通过buffer channel，防止go routine泄露
+	ch := make(chan string, numOfRunner)
+	/**
+	运行效果不同
+	1:ch := make(chan string)、
+	2:ch := make(chan string, numOfRunner)
+	*/
+	for i := 0; i < numOfRunner; i++ {
+		go func(i int) {
+			ret := runTask(i)
+			ch <- ret
+		}(i)
+	}
+	return <-ch
+}
+
+func TestFirstResponse(t *testing.T) {
+	t.Log("Before:", runtime.NumGoroutine())
+	t.Log(FirstResponse())
+	time.Sleep(time.Second * 1)
+	t.Log("After:", runtime.NumGoroutine())
+}
