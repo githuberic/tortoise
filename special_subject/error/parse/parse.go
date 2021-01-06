a0083a4952e2588202f5fb8d8afdc728afef1b8e
lgq <liugq@weipaitang.com>
2021-01-06 23:43:58 +0800

    add ss-error-parse

---
+++ special_subject/error/parse/parse.go
@@ -0,0 +1,73 @@
+package parse
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+// A ParseError indicates an error in converting a word into an integer.
+type ParseError struct {
+	// The index into the space-separated list of words.
+	Index int
+	// The word that generated the parse error.
+	Word string
+	// The raw error that precipitated this error, if any.
+	Err error
+}
+
+// String returns a human-readable error message.
+func (e *ParseError) String() string {
+	// %q 带引号的字符串输出
+	return fmt.Sprintf("pkg parse: error parsing %q as int", e.Word)
+}
+
+// Parse parses the space-separated words in in put as integers.
+func Parse(input string) (numbers []int, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			var ok bool
+			err, ok = r.(error)
+			if !ok {
+				err = fmt.Errorf("Pkg: %v", r)
+			}
+		}
+	}()
+
+	fields := strings.Fields(input)
+	numbers = fields2numbers(fields)
+	return numbers, err
+}
+
+/*
+func Parse(input string) (numbers []int, err error) {
+	defer func() {
+		switch p := recover(); p {
+		case nil:
+		case ParseError{}:
+			err = fmt.Errorf("pkg: %v", err)
+		default:
+			panic(err)
+		}
+	}()
+
+	fields := strings.Fields(input)
+	numbers = fields2numbers(fields)
+	return numbers, err
+}
+*/
+
+func fields2numbers(fields []string) (numbers []int) {
+	if len(fields) == 0 {
+		panic("no words to parse")
+	}
+
+	for idx, field := range fields {
+		num, err := strconv.Atoi(field)
+		if err != nil {
+			panic(&ParseError{idx, field, err})
+		}
+		numbers = append(numbers, num)
+	}
+	return numbers
+}
