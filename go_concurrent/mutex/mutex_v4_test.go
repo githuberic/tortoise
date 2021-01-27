97e4688c22332d41cbfcf7df6f800de6b315790e
lgq <liugq@weipaitang.com>
2021-01-27 23:18:21 +0800

    add go_concurrent-mutext-v4

---
+++ go_concurrent/mutex/mutex_v4_test.go
@@ -0,0 +1,42 @@
+package mutex
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+func TestVerifyV4(t *testing.T) {
+	var counterV4 CounterV4
+
+	var wg  sync.WaitGroup
+	wg.Add(10)
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			for j := 0; j < 100000; j++ {
+				counterV4.Incr()
+			}
+		}()
+	}
+	wg.Wait()
+	fmt.Println(counterV4.Count())
+}
+
+type CounterV4 struct {
+	mu sync.Mutex
+	count uint64
+}
+
+func (c *CounterV4) Incr() {
+	c.mu.Lock()
+	c.count++
+	c.mu.Unlock()
+}
+
+func (c *CounterV4) Count() uint64 {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.count
+}
