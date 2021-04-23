694b6b6a38501edaa9114652938bf89431c34979
lgq <liugq@weipaitang.com>
2021-04-23 23:38:01 +0800

    update poinster

--- gopl.io/ch2/pointer/main.go
+++ gopl.io/ch2/pointer/main.go
@@ -2,23 +2,26 @@ package main
 
 import "fmt"
 
-func main()  {
+func main() {
 	x := 1
+
 	// &x(x的地址)获取一个指向整形变量的指针,它的类型是整形指针(*int)
-	p := &x
-	fmt.Println(x)
-	fmt.Println(*p)
-	*p =2
-	fmt.Println(x)
+	var p *int
+	p = &x
+	fmt.Println("Address=", &p)
+	fmt.Println("Value=", *p)
+
+	*p = 2
+	fmt.Println("Value(x)=",x)
 
 	var xx, y int
 	fmt.Println(&xx == &xx, &xx == &y, &xx == nil)
 
-	v :=1
+	v := 1
 	fmt.Println(incr(&v))
 }
 
-func incr(p *int) int  {
+func incr(p *int) int {
 	*p++
 	return *p
 }
