cb85a1743a99521b59567afc132eb316b5a92ad7
lgq <liugq@weipaitang.com>
2021-09-08 23:35:13 +0800

    update slice practices

--- go_practice/ch1/slice/main_test.go
+++ go_practice/ch1/slice/main_test.go
@@ -3,31 +3,42 @@ package slice_test
 import (
 	"bytes"
 	"fmt"
+	"math/rand"
 	"testing"
+	"time"
 	"unsafe"
 )
 
 func TestSlice1(t *testing.T) {
-	foo := make([]int, 5)
-	foo[3] = 42
-	foo[4] = 100
+	var rd = rand.New(rand.NewSource(time.Now().UnixNano()))
+	var foo = make([]int, 5, 10)
+	foo[3] = rd.Int()
+	foo[4] = rd.Int()
+	Printf(foo)
 
 	bar := foo[1:4]
-	bar[1] = 99
+	bar[1] = rd.Int()
+
 	// foo 和 bar 的内存是共享的，所以，foo 和 bar 对数组内容的修改都会影响到对方
-	fmt.Println(foo)
-	fmt.Println(bar)
+	Printf(foo)
+	Printf(bar)
 }
 
+
+
 func TestSlice11(t *testing.T) {
-	arrA := make([]int, 32)
-	arrB := arrA[1:16]
+	arrA := make([]int, 10)
+	Printf(arrA)
+
 	// 对 a 做了一个 append()的操作，这个操作会让 a 重新分配内存，这就会导致 a 和 b 不再共享
-	arrA = append(arrA, 1)
-	arrA = append(arrA, 2)
+	arrA = append(arrA,1,2,3,4,5,56,57,34,68)
+	Printf(arrA)
+
+	arrB := arrA[1:5]
 	arrA[3] = 42
-	fmt.Println(arrA)
-	fmt.Println(arrB)
+	Printf(arrA)
+
+	Printf(arrB)
 }
 
 func TestSlice2(t *testing.T) {
@@ -35,36 +46,34 @@ func TestSlice2(t *testing.T) {
 	sepIndex := bytes.IndexByte(path, '/')
 	fmt.Printf("Index=%d\n", sepIndex)
 
-	dir1 := path[:sepIndex]
-	dir2 := path[sepIndex+1:]
+	var dir1 = path[:sepIndex]
+	fmt.Printf("Path=>%s,Dir1=>%s,Length=%d,Capacity=%d,Address=%v\n",string(path),string(dir1),len(dir1),cap(dir1),unsafe.Pointer(&dir1))
 
-	fmt.Println("dir1 =>", string(dir1)) //prints: dir1 => AAAA
-	t.Log("length=", len(dir1), "capacity=", cap(dir1))
-
-	fmt.Println("dir2 =>", string(dir2)) //prints: dir2 => BBBBBBBBB
-	t.Log("length=", len(dir2), "capacity=", cap(dir2))
+	var dir2 = path[sepIndex+1:]
+	fmt.Printf("Path=>%s,Dir2=>%s,Length=%d,Capacity=%d,Address=%v\n",string(path),string(dir2),len(dir2),cap(dir2),unsafe.Pointer(&dir2))
 
 	dir1 = append(dir1, "suffix"...)
-	fmt.Println("dir1 =>", string(dir1)) //prints: dir1 => AAAAsuffix
-	t.Log("length=", len(dir1), "capacity=", cap(dir1))
-
-	fmt.Println("dir2 =>", string(dir2)) //prints: dir2 => uffixBBBB
-	t.Log("length=", len(dir2), "capacity=", cap(dir2))
+	fmt.Printf("Path=>%s,Dir1=>%s,Length=%d,Capacity=%d,Address=%v\n",string(path),string(dir1),len(dir1),cap(dir1),unsafe.Pointer(&dir1))
+	fmt.Printf("Path=>%s,Dir2=>%s,Length=%d,Capacity=%d,Address=%v\n",string(path),string(dir2),len(dir2),cap(dir2),unsafe.Pointer(&dir2))
 }
 
 func TestVerify(t *testing.T) {
 	arr := []int{1, 2, 3}
-	t.Log("Before change,",arr[1],",address=",unsafe.Pointer(&arr))
+	t.Log("Before change,", arr, ",address=", unsafe.Pointer(&arr))
 	changeValue(arr...)
-	t.Log("After change,",arr[1],",address=",unsafe.Pointer(&arr))
+	t.Log("After change,", arr, ",address=", unsafe.Pointer(&arr))
 
-	a,b,c := 1,2,3
-	t.Log("Before change,",b,",address=",unsafe.Pointer(&b))
-	changeValue(a,b,c)
-	t.Log("After change,",b,",address=",unsafe.Pointer(&b))
+	a, b, c := 1, 2, 3
+	t.Log("Before change,", b, ",address=", unsafe.Pointer(&b))
+	changeValue(a, b, c)
+	t.Log("After change,", b, ",address=", unsafe.Pointer(&b))
 }
 
-func changeValue(arr ...int)  {
-	arr[1] = 0
-	fmt.Println("Change,",arr[1],",address=",unsafe.Pointer(&arr))
+func changeValue(arr ...int) {
+	arr[1] = 10
+	//fmt.Println("Change,", arr[1], ",address=", unsafe.Pointer(&arr))
+}
+
+func Printf(arr []int)  {
+	fmt.Printf("Value=>%v,Length=%d,Capacity=%d,Address=%v\n",arr, len(arr), cap(arr),unsafe.Pointer(&arr))
 }
