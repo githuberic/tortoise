d9e33eee675de96fdd8f147f0b7391bb45044fe9
lgq <liugq@weipaitang.com>
2021-10-15 15:37:40 +0800

    add channel practices

--- go_learning/ch7/util_anyone_reply/first_response_test.go
+++ go_learning/ch7/util_anyone_reply/first_response_test.go
@@ -4,18 +4,19 @@ import (
 	"fmt"
 	"runtime"
 	"testing"
-	"time"
 )
 
 func runTask(id int) string {
-	time.Sleep(10 * time.Millisecond)
+	//time.Sleep(10 * time.Millisecond)
 	return fmt.Sprintf("The result is from %d", id)
 }
 
 func FirstResponse() string {
 	numOfRunner := 10
+
 	// 通过buffer channel，防止go routine泄露
 	ch := make(chan string, numOfRunner)
+
 	/**
 	运行效果不同
 	1:ch := make(chan string)、
@@ -23,16 +24,17 @@ func FirstResponse() string {
 	*/
 	for i := 0; i < numOfRunner; i++ {
 		go func(i int) {
-			ret := runTask(i)
-			ch <- ret
+			ch <- runTask(i)
 		}(i)
 	}
 	return <-ch
+	// (此时 channel 里面 内容没有被读取完毕了，其goroutine没有释放)
 }
 
 func TestFirstResponse(t *testing.T) {
+	// runtime.NumGoroutine()
 	t.Log("Before:", runtime.NumGoroutine())
 	t.Log(FirstResponse())
-	time.Sleep(time.Second * 1)
+	//time.Sleep(time.Second * 1)
 	t.Log("After:", runtime.NumGoroutine())
 }
