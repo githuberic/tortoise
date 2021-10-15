7f9cf4329db822720940d678915cc0ff556867da
lgq <liugq@weipaitang.com>
2021-10-15 15:48:11 +0800

    add object pool

--- go_learning/ch7/obj_pool/obj_pool_test.go
+++ go_learning/ch7/obj_pool/obj_pool_test.go
@@ -1,18 +1,53 @@
 package object_pool
 
 import (
+	"errors"
 	"fmt"
 	"testing"
 	"time"
 )
 
+type ReusableObj struct {
+}
+
+type ObjPool struct {
+	bufChan chan *ReusableObj //用于缓冲可重用对象
+}
+
+func NewObjPool(numOfObj int) *ObjPool {
+	objPool := ObjPool{}
+	objPool.bufChan = make(chan *ReusableObj, numOfObj)
+	for i := 0; i < numOfObj; i++ {
+		objPool.bufChan <- &ReusableObj{}
+	}
+	return &objPool
+}
+
+func (p *ObjPool) GetObj(timeout time.Duration) (*ReusableObj, error) {
+	select {
+	case ret := <-p.bufChan:
+		return ret, nil
+	case <-time.After(timeout): //超时控制
+		return nil, errors.New("time out")
+	}
+}
+
+func (p *ObjPool) ReleaseObj(obj *ReusableObj) error {
+	select {
+	case p.bufChan <- obj:
+		return nil
+	default:
+		return errors.New("overflow")
+	}
+}
+
 func TestObjPool(t *testing.T) {
 	pool := NewObjPool(10)
 	/*
-	if err := pool.ReleaseObj(&ReusableObj{}); err != nil { //尝试放置超出池大小的对象
-		t.Error(err)
-	}*/
-	for i := 0; i < 11; i++ {
+		if err := pool.ReleaseObj(&ReusableObj{}); err != nil { //尝试放置超出池大小的对象
+			t.Error(err)
+		}*/
+	for i := 0; i < 10; i++ {
 		if v, err := pool.GetObj(time.Second * 1); err != nil {
 			t.Error(err)
 		} else {
