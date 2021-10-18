4f7292c45cb72b8b7ce0e25e1e49e2350fce95dc
lgq <liugq@weipaitang.com>
2021-10-19 00:45:37 +0800

    update itoa

--- go_learning/ch3/enum/enum_try_test.go
+++ go_learning/ch3/enum/enum_try_test.go
@@ -17,7 +17,33 @@ const (
 	Pending
 )
 
+type Allergen int
+
+const (
+	IgEggs         Allergen = 1 << iota // 1 << 0 which is 00000001
+	IgChocolate                         // 1 << 1 which is 00000010
+	IgNuts                              // 1 << 2 which is 00000100
+	IgStrawberries                      // 1 << 3 which is 00001000
+	IgShellfish                         // 1 << 4 which is 00010000
+)
+
+// 定义数量级
+type ByteSize float64
+const (
+	_           = iota             // ignore first value by assigning to blank identifier
+	KB ByteSize = 1 << (10 * iota) // 1 << (10*1)
+	MB                             // 1 << (10*2)
+	GB                             // 1 << (10*3)
+	TB                             // 1 << (10*4)
+	PB                             // 1 << (10*5)
+	EB                             // 1 << (10*6)
+	ZB                             // 1 << (10*7)
+	YB                             // 1 << (10*8)
+)
+
 func TestVerify(t *testing.T) {
 	t.Log(Wednesday)
 	t.Log(Pending)
+	t.Log(IgShellfish)
+	t.Log(MB)
 }
--- gopl.io/ch2/bit/v1/bit_v1_test.go
+++ gopl.io/ch2/bit/v1/bit_v1_test.go
@@ -6,19 +6,20 @@ import (
 )
 
 func TestVerify(t *testing.T) {
-	var x int64 = 3
+	x := 3
 	fmt.Printf("%08b\n", x)
 	fmt.Printf("%08b\n", x<<1)
 	fmt.Printf("%08b\n", x<<2)
 	fmt.Printf("%08b\n", x<<3)
 
-	var y int = 12
-	fmt.Printf("%08b\n", y<<2)
-	fmt.Printf("%d\n", y<<2)
+	y := 12
+	fmt.Printf("%08b,%d\n", y, y)
+	fmt.Printf("%08b,%d\n", y<<2, y<<2)
+	fmt.Printf("%08b,%d\n", y<<3, y<<3)
+	fmt.Printf("%08b,%d\n", y<<4, y<<4)
 
-	fmt.Printf("%08b\n", y<<3)
-	fmt.Printf("%d\n", y<<3)
-
-	fmt.Printf("%08b\n", y<<4)
-	fmt.Printf("%d\n", y<<4)
+	z := 12
+	fmt.Printf("%08b,%d\n", z, z)
+	fmt.Printf("%08b,%d\n", z>>1, z>>1)
+	fmt.Printf("%08b,%d\n", z>>2, z>>2)
 }
